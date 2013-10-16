bc0b9daff1b399915f7c6031445e2145793d19d1
Mikel Emaldi <m.emaldi@gmail.com>
2013-10-16 11:05:24 +0200

    Changed matrix order (B x A) instead (A x B)

--- alignapy.py
+++ alignapy.py
@@ -1,5 +1,6 @@
 from rdflib import Graph
 from sets import Set
+from stringdistances import substring_distance
 
 class AlignmentProcess():
     
@@ -46,17 +47,37 @@ class AlignmentProcess():
         self._bind_prefixes(self.onto2)
         
     def align(self):
+        #Parameters
+        pia = 1.0 # relation weight for name
+ 
         if self.onto1 != None and self.onto2 != None:            
             # Create property lists and matrix
             prop_list1 = self._get_properties(self.onto1)
             prop_list2 = self._get_properties(self.onto2)
-            prop_matrix = [[0 for x in xrange(len(prop_list1) + 1)] for x in xrange(len(prop_list2) + 1)]
+            prop_matrix = [[0 for x in xrange(len(prop_list2))] for x in xrange(len(prop_list1))]
              
             # Create class lists and matrix
             class_list1 = self._get_classes(self.onto1)
             class_list2 = self._get_classes(self.onto2)
-            class_matrix = [[0 for x in xrange(len(class_list1) + 1)] for x in xrange(len(class_list2) + 1)]
+            class_matrix = [[0 for x in xrange(len(class_list2))] for x in xrange(len(class_list1))]
 
-            for prop in prop_list1:
-                entity_name = self._get_entity_name(prop)
-                print entity_name
+            i = 0;
+
+            for prop1 in prop_list1:
+                j = 0;
+                entity_name1 = self._get_entity_name(prop1)
+                if entity_name1 != None:
+                    entity_name1 = entity_name1.lower()
+                
+                for prop2 in prop_list2:
+                    entity_name2 = self._get_entity_name(prop2)
+                    if entity_name2 != None:
+                        entity_name2 = entity_name2.lower()
+                    
+                    if entity_name1 != None or entity_name2 != None:                  
+                        prop_matrix[i][j] = pia + substring_distance()
+                    else:
+                        prop_matrix[i][j] = 1.0
+                    
+                    j += 1
+                i += 1
