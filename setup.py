1a50fc9f938856c59f2c30a9f9e625b54b1c9fe4
Mikel Emaldi <m.emaldi@gmail.com>
2013-11-14 17:28:02 +0100

    First commit of setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name = "Alignapy",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires=[
+                'rdflib>=4.0.1',
+                'nltk>=2.0',
+                'requests>=2.0.0',
+    ],
+    author = "Mikel Emaldi",
+    author_email = "m.emaldi@deusto.es",
+    description = "Python port of some features of Alignapi (http://alignapi.gforge.inria.fr/)",
+    license = "LGPL",
+    keywords = "rdf ontology matching linked data semantic web",
+    url = "https://github.com/memaldi/alignapy",   # project home page, if any
+)
+
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-from alignapy import NameAndPropertyAlignment, StringDistAlignment, NameEqAlignment, EditDistNameAlignment, SMOANameAlignment, SubsDistNameAlignment
+from alignapy import NameAndPropertyAlignment, StringDistAlignment, NameEqAlignment, EditDistNameAlignment, SMOANameAlignment, SubsDistNameAlignment, JWNLAlignment
 
-ap = SubsDistNameAlignment()
-ap.init('ontologies/aktors.owl', 'ontologies/bibo.owl')
+ap = JWNLAlignment()
+ap.init('http://purl.org/dc/terms/', 'http://aktors.org/ontology/portal')
 
-ap.align()
-
+try:
+    ap.align(threshold=0.7)
+except Exception as e:
+    pass
+print len(ap.cell_list)
 for cell in ap.cell_list:
     print cell.prop1, cell.prop2, cell.measure
